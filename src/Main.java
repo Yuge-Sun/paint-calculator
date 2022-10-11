60c9da2d88ae3441ba71ad2d8da8c39ec2708bc7
Yuge Sun <sun.yuge@hotmail.com>
2022-10-11 12:48:35 +0100

    added loops and obstacle calculation

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,24 +8,35 @@ public class Main {
         double height = 3.0;
 */
         System.out.print("Enter the height of the room: \n");
-        Scanner myObj = new Scanner(System.in);
-        double height = myObj.nextDouble();
-
+        Scanner userinput = new Scanner(System.in);
+        double height = userinput.nextDouble();
         System.out.print("Enter the number of walls: \n");
-        int numberofwalls = myObj.nextInt();
+        int numberofwalls = userinput.nextInt();
         double totalarea = 0;
         for (int i = 1 ; i <= numberofwalls ; i++) {
             System.out.print("Enter the length of wall " + i + ":\n");
-            double length = myObj.nextDouble();
-//            System.out.print("Enter the width of wall " + i + ":\n");
-//            double width = myObj.nextDouble();
+            double length = userinput.nextDouble();
             totalarea = totalarea + length*height;
+            System.out.print("Enter the number of obstacles on this wall: \n");
+            int obstaclenum = userinput.nextInt();
+            double obstacles[] = new double[obstaclenum];
+            for (int j = 0 ; j < obstaclenum ; j++) {
+                int k=j+1;
+                System.out.print("Enter the length of obstacle " + k + ":\n");
+                double obstaclelength = userinput.nextDouble();
+                System.out.print("Enter the width of obstacle " + k + ":\n");
+                double obstaclewidth = userinput.nextDouble();
+                obstacles[j] = obstaclelength*obstaclewidth;
+                totalarea = totalarea - obstacles[j];
+            }
+//            System.out.print("Enter the width of wall " + i + ":\n");
+//            double width = userinput.nextDouble();
         }
         System.out.print("Enter the area of the ceiling:\n");
-        double ceilingarea = myObj.nextDouble();
+        double ceilingarea = userinput.nextDouble();
         totalarea = totalarea + ceilingarea;
         System.out.print("Please Select a type of paint:\nType 1: £10.00/bucket, Type 2: £19.99/bucket, Type 3: £50.00/bucket\n");
-        int painttype = myObj.nextInt();
+        int painttype = userinput.nextInt();
         int paintprice = 0;
         int paintpriceA = 1000;
         int paintpriceB = 1999;
